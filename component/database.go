59aab575f0911e9d9ab6dc129442299dd00f2dfd
clement4al <michaelokonclement@gmail.com>
2025-08-29 12:32:41 +0100

    added database package to be able to conn to db

--- component/database.go
+++ component/database.go
@@ -8,23 +8,32 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
+var Database *pgx.Conn
+
 func Startdb() {
 	// Connection string
 	dsn := "postgres://postgres:test1234@localhost:5432/postgres"
 
-	// Connect to PostgreSQL
+	// Connection to PostgreSQL
 	conn, err := pgx.Connect(context.Background(), dsn)
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
-	defer conn.Close(context.Background())
 
-	// Test query
-	var greeting string
-	err = conn.QueryRow(context.Background(), "SELECT 'Hello, PostgreSQL from Go!'").Scan(&greeting)
-	if err != nil {
-		log.Fatalf("Query failed: %v\n", err)
+	// Save connection in the global variable
+	Database = conn
+
+	fmt.Println("Database connection created successfully..")
+}
+
+// CloseDB closes the global database connection
+func CloseDB() {
+	if Database != nil {
+		err := Database.Close(context.Background())
+		if err != nil {
+			log.Printf("Error closing database: %v\n", err)
+		} else {
+			fmt.Println("Database connection closed successfully..")
+		}
 	}
-
-	fmt.Println(greeting)
 }
--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "Go/component"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"Go/component"
+)
 
 func main() {
 	// component.StartRabbitMQ()
 	component.Startdb()
+	defer component.CloseDB()
+	//exmaple query
+	var sweepapp string
+	err := component.Database.QueryRow(context.Background(), "SELECT 'hello from the Database'").Scan(&sweepapp)
+	if err != nil {
+		log.Fatalf("Query failed: %v\n", err)
+	}
+
+	fmt.Println(sweepapp)
 }
